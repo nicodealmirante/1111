89eaee33e8ddbd9d67585cba2b89d5fd36f8188b
nicodealmirante <nicolas.e.luna@gmail.com>
2024-05-04 03:17:05 -0300

    jkn

--- app.js
+++ app.js
@@ -796,92 +796,8 @@ const chatwoot = new ChatwootClass({
 
           queue.enqueue(async () => {
             try {
-              const attachment = [];
-      
-              if (payload?.body.includes("_event_media_")) {
-      
-                const mime_type = payload.mime_type;
-                const ext = mimeType.extension(`${mime_type}`);
-      
-                const buffer = await downloadMediaMessage(payload, "buffer");
-      
-                const fileName = `file-${Date.now()}.${ext}`;
-                const pathFile = `${process.cwd()}/public/${fileName}`;
-                await fs.writeFile(pathFile, buffer);
-      
-                attachment.push(pathFile);
-      
-      
-                await handlerMessage({
-                    type: payload.mime_type,
-                    phone: nuevoOrden,
-                    phonecrypt: numeroEncriptado,
-                    name: payload.pushName,
-                    message: payload.caption ? payload.caption : "",
-                    attachment,
-                    mode: 'outgoing'
-                }, chatwoot)
-      
-      
-              } else if (payload?.body.includes("_event_document_")) {
-                function obtenerExtension(nombreArchivo) {
-                  return nombreArchivo.split(".").pop();
-                }
-      
-                const mime_type = payload.mime_type;
-                const nombre = payload.filename;
-                console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",nombre);
-                ext = obtenerExtension(nombre);
-      
-                buffer = await downloadMediaMessage(payload, "buffer");
-      
-                const fileName = `${nombre}`;
-                const pathFile = `${process.cwd()}/public/${fileName}`;
-                await fs.writeFile(pathFile, buffer);
-      
-                attachment.push(pathFile);
-      
-                
-                await handlerMessage({
-                    type: payload.type,
-                    phone: nuevoOrden,
-                    phonecrypt: numeroEncriptado,
-                    name: payload.pushName,
-                    message: payload.caption ? payload.caption : (payload.filename ? payload.filename : ""),
-                    attachment,
-                    mode: 'outgoing'
-                }, chatwoot)
-      
-              } else if (payload?.body.includes("_event_audio_")) {
-      
-                const mime_type = payload.mime_type;
-                const ext = mimeType.extension(`${mime_type}`);
-      
-                buffer = await downloadMediaMessage(payload, "buffer");
-      
-                const fileName = `file-${Date.now()}.${ext}`;
-                const pathFile = `${process.cwd()}/public/${fileName}`;
-                await fs.writeFile(pathFile, buffer);
-      
-                attachment.push(pathFile);
-      
-                
-                await handlerMessage({
-                    type: payload.mime_type,
-                    phone: nuevoOrden,
-                    phonecrypt: numeroEncriptado,
-                    name: payload.pushName,
-                    message: payload.caption ? payload.caption : (payload.filename ? payload.filename : ""),
-                    attachment,
-                    mode: 'outgoing'
-                }, chatwoot)
-              } else {
-      
-                
-                // Proceso para manejar otros tipos de eventos
-                // Aquí puedes manejar mensajes que no sean media o documentos
-                const genericMessage = payload.body; // Mensaje original
-      
+           
+            
                 await handlerMessage(
                   {
                     type: payload.type,
@@ -893,18 +809,18 @@ const chatwoot = new ChatwootClass({
                     mode: "outgoing",
                   },
                   chatwoot
-                );
-              }
+                )
+          
             } catch (err) {
               console.log("ERROR123", err);
             }
-          });
+          })
       
-          
+        })
 
         
      
-        })
+        
 
 
     }

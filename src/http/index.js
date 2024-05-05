e1d4502e851bbb4797f4c226dcdb952f36b86355
nicodealmirante <nicolas.e.luna@gmail.com>
2024-05-05 16:52:28 -0300

    adsasdsa

--- src/http/index.js
+++ src/http/index.js
@@ -72,16 +72,7 @@ class ServerHttp {
 
         try {
 
-            const mapperAttributes = body?.changed_attributes?.map((a) => Object.keys(a)).flat(2)
 
-            /**
-             * Esta funcion se encarga de agregar o remover el numero a la blacklist
-             * eso quiere decir que podemos hacer que el chatbot responda o no
-             * para que nos sirve, para evitar que el chatbot responda mientras
-             * un agente humano esta escribiendo desde chatwoot
-             */
-            if (body?.event === 'conversation_updated' && mapperAttributes.includes('assignee_id')) {
-                const phone = body?.meta?.sender?.phone_number.replace('+', '')
                 const idAssigned = body?.changed_attributes[0]?.assignee_id?.current_value ?? null
                 console.log('CASU')
 
@@ -93,9 +84,6 @@ class ServerHttp {
 
                     bot.dynamicBlacklist.remove(numeroDesencriptado)
                 }
-                res.send('ok')
-                return
-            }
 
             /**
              * La parte que se encarga de determinar si un mensaje es enviado al whatsapp del cliente

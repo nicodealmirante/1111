d7d6fff12481850a839f7dd7fc76d41a90d35992
nicodealmirante <nicolas.e.luna@gmail.com>
2024-05-05 13:57:16 -0300

    nb b

--- app.js
+++ app.js
@@ -798,32 +798,7 @@ const chatwoot = new ChatwootClass({
          * Los mensajes salientes (cuando el bot le envia un mensaje al cliente ---> )
          */
         bot.on('send_message', (payload) => {
-           if (payload?.body.includes("_event_media_")) {
-
-          const mime_type = payload.mime_type;
-          const ext = mimeType.extension(`${mime_type}`);
-
-          const buffer = await downloadMediaMessage(payload, "buffer");
-
-          const fileName = `file-${Date.now()}.${ext}`;
-          const pathFile = `${process.cwd()}/public/${fileName}`;
-          await fs.writeFile(pathFile, buffer);
-
-          attachment.push(pathFile);
-
-
-          await handlerMessage({
-              type: payload.mime_type,
-              phone: nuevoOrden,
-              phonecrypt: numeroEncriptado,
-              name: payload.pushName,
-              message: payload.caption ? payload.caption : "",
-              attachment,
-              mode: 'incoming'
-          }, chatwoot)
-
-
-        } else{
+         
           console.log("holaaaaaaaaa outgoing", payload);
             queue.enqueue(async () => {
                 await handlerMessage({
